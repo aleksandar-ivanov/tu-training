3fdef06d16ee9cffde8c14f8e2767e9c2710a3fa
aleksandar.ivanov <kapivanov93@gmail.com>
2020-04-08 02:16:36 +0300

    Task one V3 with css classes

--- validation.php
+++ validation.php
@@ -1,16 +1,43 @@
+<head>
+    <style>
+        .error {
+            color: red;
+            font-weight: 600;
+        }
+
+        .success {
+            color: green;
+            font-weight: 700;
+        }
+    </style>
+</head>
 <?php
 
-$name = isset($_POST['name']) ? $_POST['name'] : null;
+const MIN_CHARS = 3;
+const MAX_CHARS = 50;
+
+$error = null;
+$success = null;
+
+$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+$nameLength = mb_strlen($name);
 
 if (!$name) {
-    echo "Name is required";
-    exit;
+    $error = 'Name is required';
+} else {
+    if ($nameLength < MIN_CHARS || $nameLength > MAX_CHARS) {
+        $error = sprintf("Name must be between %d and %d characters!", MIN_CHARS, MAX_CHARS);
+    } else {
+        $success = "Name: {$name} is correct";
+    }
 }
 
-if (mb_strlen($name) < 3 || mb_strlen($name) > 5) {
-    echo "Name must be between 3 and 5 characters!";
-    exit;
+if ($success) {
+    echo "<p class='success'>{$success}</p>";
+} elseif ($error) {
+    echo "<p class='error'>{$error}</p>";
 }
 
-echo "Name: {$name} is correct";
+
+
 
